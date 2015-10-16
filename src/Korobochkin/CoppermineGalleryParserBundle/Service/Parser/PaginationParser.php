3b27e4db5d4f9c687ceb6006e6f739f5b2a73f03
Kolya Korobochkin <me@korobochkin.com>
2015-10-16 21:55:11 +0300

    Pagination parser class

---
+++ src/Korobochkin/CoppermineGalleryParserBundle/Service/Parser/PaginationParser.php
@@ -0,0 +1,29 @@
+<?php
+namespace Korobochkin\CoppermineGalleryParserBundle\Service\Parser;
+
+class Pagination
+{
+    public function getCustomPage() {
+
+    }
+
+    public function getFirstPage() {
+
+    }
+
+    public function getLastPage() {
+
+    }
+
+    public function getNextPage() {
+
+    }
+
+    public function getAllPages() {
+
+    }
+
+    public function pageExist() {
+
+    }
+}
