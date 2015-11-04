35792637d0b1087b6f5ec49c66ad3786cc83318d
Kolya Korobochkin <me@korobochkin.com>
2015-11-05 00:23:21 +0300

    Подключаем штуку для работы со строчками (Stringy)

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new Korobochkin\CoppermineGalleryParserBundle\KorobochkinCoppermineGalleryParserBundle(),
+            new Stringy\Stringy(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
