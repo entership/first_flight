5a8ca394cd7dfc16d17af5f9aa5c3cd47f086d72
Kolya Korobochkin <me@korobochkin.com>
2015-11-04 22:53:59 +0300

    Небольшой реформат кода

--- src/Korobochkin/CoppermineGalleryParserBundle/Service/Parser.php
+++ src/Korobochkin/CoppermineGalleryParserBundle/Service/Parser.php
@@ -2,6 +2,7 @@
 namespace Korobochkin\CoppermineGalleryParserBundle\Service;
 
 use DOMElement;
+
 //use Etki\SranyParserBundle\Service\Parser\Selectors;
 //use Etki\SranyParserBundle\Service\Parser\Urls;
 use GuzzleHttp\Client;
@@ -11,22 +12,21 @@ use Monolog\Logger;
 use Symfony\Component\DomCrawler\Crawler;
 use Symfony\Component\Validator\Constraints\Url;
 
-/**
- * Class AlbumParser
- * @package Korobochkin\CoppermineGalleryParserBundle\Service
- */
-class Parser {
+class Parser
+{
 
     private $logger;
 
     private $guzzle;
 
-    public function  __construct(Client $client, Logger $logger) {
+    public function  __construct(Client $client, Logger $logger)
+    {
         $this->logger = $logger;
         $this->guzzle = $client;
     }
 
-    public function run($url) {
+    public function run($url)
+    {
         $url = parse_url($url);
         // Ищем тип страницы по $url
 
