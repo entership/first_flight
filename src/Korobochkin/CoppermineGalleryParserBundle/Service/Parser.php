03f874420fc2ea89dea6b73c8706d87db63f1635
Kolya Korobochkin <me@korobochkin.com>
2015-11-04 22:51:59 +0300

    Выкидываем все нафиг

--- src/Korobochkin/CoppermineGalleryParserBundle/Service/Parser.php
+++ src/Korobochkin/CoppermineGalleryParserBundle/Service/Parser.php
@@ -21,133 +21,15 @@ class Parser {
 
     private $guzzle;
 
-    private $urlComponents;
-
-    private $albumPages;
-
     public function  __construct(Client $client, Logger $logger) {
         $this->logger = $logger;
         $this->guzzle = $client;
     }
 
     public function run($url) {
-        // Выбрать нужную сущность
-        $this->urlComponents = parse_url($url);
+        $url = parse_url($url);
+        // Ищем тип страницы по $url
 
-        if(!empty($this->urlComponents['path'])) {
-            //$pathLenght = strlen($this->urlComponents['path']);
-
-            if($this->checkPath(Urls::ALBUM)) {
-
-            }
-            elseif($this->checkPath(Urls::CATEGORY) && $this->checkQuery(Urls::CATEGORY_QUERY)) {
-                $this->logger->error('Categories unsupported right now.');
-            }
-            elseif($this->checkPath(Urls::GALLERY)) {
-                $this->logger->error('Galleries unsupported right now.');
-            }
-        }
-        $this->logger->error('Unknown URL received. Looks like it is not a Copppermine gallery (or unkown version of Coppermine gallery).');
-    }
-
-    public function parse($url) {
-        $this->urlComponents = parse_url($url);
-        if(!$this->checkPath()) {
-            $this->logger->error('Unknown URL received.');
-            return;
-        }
-
-        if(empty($this->urlComponents['host']) || empty($this->urlComponents['scheme'])) {
-            $this->logger->info('Can\'t resolve Host or Scheme.');
-            return;
-        }
-
-        $this->logger->info(
-            'Resolve Host and Schema.',
-            ['host' => $this->urlComponents['host'], 'schema' => $this->urlComponents['scheme']]
-        );
-
-        // TODO: Не опасно ли передавать сюда пользовательский ввод? Может нужно сделать экраницазацию?
-        $this->logger->info(
-            'Try to fetch entered URL.',
-            ['url' => $url]
-        );
-
-        $response = $this->guzzle->get($url);
-
-        /*
-         * TODO: А разве Кравлер не нужно ставить отдельным компонентом?
-         * В документации написано надо ставить, но судя по всему он уже есть в текущей Симфонии
-         */
-        $crawler = new Crawler((string) $response->getBody());
-
-        // А может мы должны просто попробовать запросить page=2, page=3... и если получаем 404, то все,
-        // все страницы получили. Вместо того, чтобы выдерать из DOM ссылки.
-        // Правда 404 не гарант того, что страницы нет в реальном мире :) (возможно, сервер глюканет и т. п.)
-        // $pageLinks =
-        $this->albumPages = [];
-        $crawler
-            ->filter(Selectors::ALBUM_PAGINATION_NUMBER_LINKS)
-            ->each(
-                function($node, $i) {
-                    $albumPage = parse_url($node->attr('href'));
-                    parse_str($albumPage['query'], $albumPage);
-                    $this->albumPages[] = $albumPage['page'];
-                }
-            );
-    }
-
-    /**
-     * Check if entered URL end with album path.
-     * Based on http://stackoverflow.com/questions/619610/whats-the-most-efficient-test-of-whether-a-php-string-ends-with-another-string
-     *
-     * @return bool
-     */
-    private function checkPath($pathToCheck) {
-        // We have a path
-        if(!empty($this->urlComponents['path'])) {
-            $pathLenght = strlen($this->urlComponents['path']);
-            $pathToCheckLenght = strlen($pathToCheck);
-
-            // If entered path smaller than album's URL
-            if($pathToCheckLenght > $pathLenght) {
-                return false;
-            }
-
-            // Check if the end of the path match to album URL
-            // TODO: case sensitive or not?
-            return !(bool)substr_compare(
-                $this->urlComponents['path'],
-                Parser\Urls::ALBUM,
-                $pathLenght - $pathToCheckLenght,
-                $pathToCheckLenght
-            );
-        }
-        return false;
-    }
-
-    private function checkQuery($params) {
-        if(!empty($this->urlComponents['query'])) {
-            if(is_string($this->urlComponents['query'])) {
-                parse_str($this->urlComponents['query'], $this->urlComponents['query']);
-            }
-
-            if(is_array($this->urlComponents['query'])) {
-                foreach($params as $param) {
-                    if(!empty($this->urlComponents['query'][$param])) {
-                        continue;
-                    }
-                    else {
-                        return false;
-                    }
-                }
-                return true;
-            }
-            else {
-                $this->logger->error('Unknown variable type with query parameters.');
-                return false;
-            }
-        }
-        return false;
+        // Запуск specificParser с $url и типом
     }
 }
