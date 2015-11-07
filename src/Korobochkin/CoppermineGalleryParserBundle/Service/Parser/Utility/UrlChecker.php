2a1c44d2efe95869f48fda33a1160378c44389c9
Kolya Korobochkin <me@korobochkin.com>
2015-11-08 01:50:29 +0300

    Делаем функцию для проверки альбом ли это

--- src/Korobochkin/CoppermineGalleryParserBundle/Service/Parser/Utility/UrlChecker.php
+++ src/Korobochkin/CoppermineGalleryParserBundle/Service/Parser/Utility/UrlChecker.php
@@ -1,17 +1,38 @@
 <?php
 namespace Korobochkin\CoppermineGalleryParserBundle\Service\Parser\Utility;
 
+use Korobochkin\CoppermineGalleryParserBundle\Service\Parser\Urls;
+use Stringy\Stringy;
+use Symfony\Component\Validator\Exception\InvalidArgumentException;
+
 class UrlChecker
 {
     private $urlTypes = array(
-        'gallery',
-        'category',
-        'album',
+        'gallery' => false,
+        'category' => false,
+        'album' => false,
     );
+
+    private $urlType = NULL;
 
     public function analyzeUrl($url)
     {
-        return true;
+        if($this->isAlbum($url)) {
+            $this->setUrlType('album');
+            return $this->urlType;
+        }
+        return NULL;
+    }
+
+    private function setUrlType($type) {
+        if( isset( $this->urlTypes[$type] ) ) {
+            $this->urlTypes[$type] = true;
+            $this->urlType = $type;
+        }
+        else {
+            // TODO: какую именно ошибку тут выдавать?
+            throw new InvalidArgumentException();
+        }
     }
 
     private function isGallery($url)
@@ -26,6 +47,15 @@ class UrlChecker
 
     private function isAlbum($url)
     {
+        if ( ! empty( $url['path'] )) {
+            // TODO: как правильно вызывать Stringy чтобы не создавать 100 экземпляров внутри кода?
+            $stringy = new Stringy();
+            $result  = $stringy->endsWith(Urls::ALBUM);
+            if ($result === true) {
+                return true;
+            }
+        }
+
         return false;
     }
 }
